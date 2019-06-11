d949a5d61551dbcf47bc333b6124d1236efdc69b
Kamilox09 <ehe241@gmail.com>
2019-06-11 14:01:32 +0200

    Added UserRepository

---
+++ src/main/java/com/java/projects/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.java.projects.repository;
+
+import com.java.projects.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByUsername(String username);
+}
