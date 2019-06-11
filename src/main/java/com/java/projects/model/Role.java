ffcfefe46385b284883ce427bcbe25b54465a625
Kamilox09 <ehe241@gmail.com>
2019-06-11 13:33:28 +0200

    Added Role entity

---
+++ src/main/java/com/java/projects/model/Role.java
@@ -0,0 +1,41 @@
+package com.java.projects.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "role")
+    private List<User> users;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
