2868dd99816a08ac8194ffff328f52baddb19ed9
Kamilox09 <ehe241@gmail.com>
2019-06-11 13:58:35 +0200

    Added RoleRepository and RoleService

---
+++ src/main/java/com/java/projects/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.java.projects.repository;
+
+import com.java.projects.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> getRoleByName(String name);
+}
---
+++ src/main/java/com/java/projects/service/RoleService.java
@@ -0,0 +1,20 @@
+package com.java.projects.service;
+
+import com.java.projects.model.Role;
+import com.java.projects.repository.RoleRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleService {
+
+    private final RoleRepository roleRepository;
+
+    public RoleService(RoleRepository roleRepository){
+        this.roleRepository = roleRepository;
+    }
+
+    public Role getUserRole() throws Exception{
+        return roleRepository.getRoleByName("ROLE_USER")
+                .orElseThrow(()-> new Exception("Nie znaleziono roli użytkownika"));
+    }
+}
